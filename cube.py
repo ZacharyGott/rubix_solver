5c4546961c5bc406cabbf82260b73bea80d4f687
ZacharyGott <69654157+ZacharyGott@users.noreply.github.com>
2022-06-15 19:44:56 -0400

    init

---
+++ cube.py
@@ -0,0 +1,41 @@
+
+class Cube:
+
+    def __init__(self, w, b, r, g, o, y):
+        self.white_side = w
+        self.blue_side = b
+        self.red_side = r
+        self.green_side = g
+        self.orange_side = o
+        self.yellow_side = y
+        pass
+
+    def white_clockwise(self):
+        pass
+
+    def white_counterclockwise(self):
+        pass
+
+    def blue_clockwise(self):
+        pass
+
+    def blue_counterclockwise(self):
+        pass
+
+    def red_clockwise(self):
+        pass
+
+    def white_clockwise(self):
+        pass
+
+    def white_clockwise(self):
+        pass
+
+    def white_clockwise(self):
+        pass
+
+    def white_clockwise(self):
+        pass
+
+    def white_clockwise(self):
+        pass
